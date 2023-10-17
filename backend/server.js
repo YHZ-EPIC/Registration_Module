8d37fab3582c7790d32bf5ed656cfab71387b41a
Yasir Hussain <59396511+YHZ-EPIC@users.noreply.github.com>
2023-10-17 21:12:46 +0500

    Try 3 : Port 5K Issue

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ dotenv.config();
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 import userRoutes from "./routes/userRoutes.js";
 
